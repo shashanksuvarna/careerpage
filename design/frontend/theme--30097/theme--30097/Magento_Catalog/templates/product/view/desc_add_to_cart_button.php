271754ddac8b4dbea53a9fd0443f1d848cc7eb75
pooja <emb-poojkan@embitel.com>
2023-05-11 17:50:38 +0530

    final submission

--- design/frontend/theme--30097/theme--30097/Magento_Catalog/templates/product/view/desc_add_to_cart_button.php
+++
@@ -1,2 +0,0 @@
-<button type="submit"title="<?php /* @escapeNotVerified */ echo $buttonTitle ?>"class="btn btn-default action primary tocart"id="product-addtocart-button">
-<span><?php /* @escapeNotVerified */ echo $buttonTitle ?></span> </button>
--- design/frontend/theme--30097/theme--30097/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
- \Magento\Framework\Component\ComponentRegistrar::register(
- \Magento\Framework\Component\ComponentRegistrar::THEME,
-'frontend/theme--30097/theme--30097',
-__DIR__
-);
